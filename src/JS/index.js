fe5007ea330c67a9a9d7b7d669f13c83230299a3
Alex8804 <128159385+Alex8804@users.noreply.github.com>
2023-09-01 18:04:09 +0200

    upd 11

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,57 +1,104 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
 // Functions
 import { serchImgs, incrementPage, resetPage } from './search-api';
 // Variables
 import { currentPage, limitPerPage } from './search-api';
+
+Notify.init({
+  fontSize: '18px',
+  width: '350px',
+  timeout: 5000,
+});
 
 const elements = {
   form: document.querySelector('#search-form'),
   searchBtn: document.querySelector('.search-btn'),
   gallery: document.querySelector('.gallery'),
   loadMoreBtn: document.querySelector('.load-more'),
+  loader: document.querySelector('.loader'),
 };
+
+const lightBoxOpt = {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+};
+
+const simpleGallery = new SimpleLightbox('.gallery a', lightBoxOpt);
 
 let searchValue = '';
 
-elements.form.addEventListener('submit', onSearchBtnClick);
+elements.form.addEventListener('submit', onSearchClick);
 
-function onSearchBtnClick(evt) {
+function onSearchClick(evt) {
   evt.preventDefault();
   elements.gallery.innerHTML = '';
   resetPage();
+  elements.loader.classList.remove('js-visible');
+  elements.loadMoreBtn.classList.add('js-visible');
 
   searchValue = evt.currentTarget.elements.searchQuery.value;
 
   serchImgs(searchValue)
     .then(data => {
+      elements.loader.classList.add('js-visible');
+
       if (data.total === 0) {
         Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
+      } else if (data.total <= limitPerPage) {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
+        elements.gallery.innerHTML = createMarkup(data.hits);
+        simpleGallery.refresh();
       } else {
+        Notify.success(`Hooray! We found ${data.totalHits} images.`);
         elements.gallery.innerHTML = createMarkup(data.hits);
         elements.loadMoreBtn.classList.remove('js-visible');
         elements.loadMoreBtn.addEventListener('click', onLoadBtnClick);
+        simpleGallery.refresh();
       }
     })
-    .catch(error => console.log(error.message));
+    .catch(error => {
+      elements.loader.classList.add('js-visible');
+      Notify.failure(error.message);
+    });
 }
 
 function onLoadBtnClick(evt) {
   evt.preventDefault();
   incrementPage();
+  elements.loadMoreBtn.classList.add('js-visible');
 
   serchImgs(searchValue, currentPage)
     .then(data => {
       let maxPage = Math.ceil(data.totalHits / limitPerPage);
-      console.log(maxPage);
 
       if (currentPage === maxPage) {
+        elements.gallery.insertAdjacentHTML(
+          'beforeend',
+          createMarkup(data.hits)
+        );
+
+        simpleGallery.refresh();
+
         elements.loadMoreBtn.classList.add('js-visible');
-        console.log('no more');
+
+        Notify.info(
+          "We're sorry, but you've reached the end of search results."
+        );
+      } else {
+        elements.gallery.insertAdjacentHTML(
+          'beforeend',
+          createMarkup(data.hits)
+        );
+        simpleGallery.refresh();
+        elements.loadMoreBtn.classList.remove('js-visible');
       }
 
-      elements.gallery.insertAdjacentHTML('beforeend', createMarkup(data.hits));
+      smoothScroll();
     })
     .catch(err => console.log(err));
 }
@@ -68,7 +115,7 @@ function createMarkup(arr) {
         comments,
         downloads,
       }) => `<div class="photo-card">
-    <img src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="200" />
+      <a href="${largeImageURL}"><img class="photo" src="${webformatURL}" alt="${tags}" loading="lazy" width="320" height="200" /></a>
     <div class="info">
       <p class="info-item">
         <b>Likes: ${likes}</b>
@@ -86,4 +133,15 @@ function createMarkup(arr) {
   </div>`
     )
     .join('');
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
